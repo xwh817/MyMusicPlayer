54ec8a73378d21e5c3a8642612476f7c1e6c19b2
xwh817 <xwh817@163.com>
2019-06-17 16:25:02 +0800

    fragment+ViewPager leakCanary内存泄漏提示

--- app/src/main/java/xwh/player/music/adapter/HomeViewPagerAdapter.java
+++ app/src/main/java/xwh/player/music/adapter/HomeViewPagerAdapter.java
@@ -1,15 +1,18 @@
 package xwh.player.music.adapter;
 
-import java.util.List;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import java.util.List;
 
 /**
  * Created by xwh on 2019/6/3.
  */
-public class HomeViewPagerAdapter extends FragmentPagerAdapter {
+public class HomeViewPagerAdapter extends FragmentStatePagerAdapter {
 
 	private List<Fragment> mFragments;
 
@@ -28,5 +31,8 @@ public class HomeViewPagerAdapter extends FragmentPagerAdapter {
 		return mFragments.size();
 	}
 
-
+	@Override
+	public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+		//super.destroyItem(container, position, object);
+	}
 }
