f3f25c2fba0fa9bd5a7a9f41e8e0dc08f98f3f0c
shine <shine@turingcat.com>
2019-07-11 17:19:30 +0800

    add icons

--- musiclib/src/main/java/xwh/lib/music/dao/DaoManager.java
+++ musiclib/src/main/java/xwh/lib/music/dao/DaoManager.java
@@ -3,10 +3,14 @@ package xwh.lib.music.dao;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
+import org.greenrobot.greendao.query.QueryBuilder;
+
 import xwh.lib.music.entity.DaoMaster;
 import xwh.lib.music.entity.DaoSession;
 
 public class DaoManager {
+
+    private static DaoSession daoSession;
 
     /**
      * 初始化GreenDao
@@ -18,11 +22,17 @@ public class DaoManager {
         daoSession = daoMaster.newSession();
     }
 
-    private static DaoSession daoSession;
     public static DaoSession getDaoSession() {
         return daoSession;
     }
 
 
+    //是否开启Log
+    public void setDebugMode(boolean flag) {
+        //MigrationHelper.DEBUG = true;//如果查看数据库更新的Log，请设置为true
+        QueryBuilder.LOG_SQL = flag;
+        QueryBuilder.LOG_VALUES = flag;
+
+    }
 
 }
